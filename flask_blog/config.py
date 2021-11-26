396f8c6adabd54ab3c4c9d587191b02c1ea501bd
Davi Feliciano <dfeliciano37@gmail.com>
2021-11-25 22:20:57 -0300

    Moving sensitive information to a config file

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,14 +1,16 @@
-import os
+import json
+
+with open("/etc/flask_blog_config.json") as config_file:
+    config = json.load(config_file)
 
 
 class Config:
-    SECRET_KEY = os.environ.get("FLASK_BLOG_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("FLASK_BLOG_SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = config.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get("FLASK_BLOG_MAIL_SERVER")
+    MAIL_SERVER = config.get("MAIL_SERVER")
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_DEFAULT_SENDER = "noreply@flaskblogdemo.com"
-    MAIL_USERNAME = os.environ.get("FLASK_BLOG_EMAIL_USER")
-    MAIL_PASSWORD = os.environ.get("FLASK_BLOG_EMAIL_PWD")
+    MAIL_USERNAME = config.get("EMAIL_USER")
+    MAIL_PASSWORD = config.get("EMAIL_PWD")
